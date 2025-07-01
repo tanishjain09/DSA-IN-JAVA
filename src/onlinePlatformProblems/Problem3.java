c1a0ba876601fbf15ec1029248f554a739840ed3
tanishjain09 <tanish.jain.0905@gmail.com>
2025-07-01 09:28:07 +0530

    adding problem 3&4 file

---
+++ src/onlinePlatformProblems/Problem3.java
@@ -0,0 +1,24 @@
+package onlinePlatformProblems;
+//Question -> leetcode 744
+
+public class Problem3 {
+    public static void main(String[] args) {
+        char[] letter = {'c', 'f', 'j'};
+        System.out.println(solution(letter,'a'));
+    }
+    static char solution(char[] letter,char target){
+        int start = 0;
+        int end = letter.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > letter[mid]){
+                start = mid + 1;
+            } else if (target < letter[mid]) {
+                end = mid - 1;
+            }
+        }
+        return letter[start % letter.length];
+    }
+}
---
+++ src/onlinePlatformProblems/Problem4.java
@@ -0,0 +1,52 @@
+package onlinePlatformProblems;
+//Question -> Leetcode 34
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 7, 7, 8, 8, 10};
+        System.out.println(Arrays.toString(solution(nums,10)));
+    }
+
+    static int[] solution(int[] nums, int target){
+
+        int[] ans = {-1, -1};
+
+        //check for fist occurrence if target first
+        int start = search(nums,target,true);
+        int end = search(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    //this function just return the index value of target
+    static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]){
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            }else{
+                //potential ans found
+                ans = mid;
+                if ( findStartIndex ){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
