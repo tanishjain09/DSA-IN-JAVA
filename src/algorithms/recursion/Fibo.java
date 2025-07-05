0f22a4a43f907ac9fa0a54b9a61718975b0a1eef
tanishjain09 <tanish.jain.0905@gmail.com>
2025-07-05 21:45:17 +0530

    learn recursion

--- src/algorithms/recursion/Fibo.java
+++ src/algorithms/recursion/Fibo.java
@@ -4,14 +4,21 @@ public class Fibo {
     public static void main(String[] args) {
 //        System.out.println(fibo(4));
 
-        for(int i =1 ;i<4; i++){
-            System.out.println(fibo(i));
-        }
+//        for(int i =0 ;i<11; i++){
+//            System.out.println(fiboFormula(i));
+//        }
+
+        System.out.println(fiboFormula(55));
     }
     static int fibo(int n){
         if(n < 2){
             return n;
         }
         return fibo(n-1) + fibo(n-2);
+    }
+
+    static long fiboFormula(int n){
+
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
     }
 }
