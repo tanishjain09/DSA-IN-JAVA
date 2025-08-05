c59179761714fd6f80ab434a56ba8b250cb25ddb
tanishjain09 <tanish.jain.0905@gmail.com>
2025-08-05 10:00:35 +0530

    dynamic queue

---
+++ src/dataStructures/Queue/CRUDoperation/DynamicQueue.java
@@ -0,0 +1,28 @@
+package dataStructures.Queue.CRUDoperation;
+
+public class DynamicQueue extends CircularQueue{
+    public DynamicQueue() {
+        super();
+    }
+
+    public DynamicQueue(int size) {
+        super(size);
+    }
+
+    //revision for this
+    @Override
+    public boolean insert(int item) {
+        if (this.isfull()){
+            int[] temp = new int[data.length];
+
+            for(int i = 0; i < data.length; i++){
+                temp[i] = data[(front+i) % data.length];
+            }
+            front = 0;
+            end = data.length;
+            data = temp;
+        }
+
+        return super.insert(item);
+    }
+}
