ea49d6b4321743300b1b72a10b07f55da3548725
tanishjain09 <tanish.jain.0905@gmail.com>
2025-08-28 21:13:48 +0530

    some changes while revising

---
+++ src/algorithms/Backtracking/CombinationsLeetcode.java
@@ -0,0 +1,27 @@
+package algorithms.Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationsLeetcode {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        combineSubset(1, n, k, ans, new ArrayList<>());
+        return ans;
+    }
+    private void combineSubset(int index, int n, int k, List<List<Integer>> ans, List<Integer> list){
+        if(k == 0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int i = index; i <= n; i++){
+            list.add(i);
+            combineSubset(i+1, n,k-1, ans,list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
