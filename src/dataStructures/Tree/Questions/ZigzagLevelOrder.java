66dd5bb3e49ebd4f2093390c7ba1c05cfbd93cb2
tanishjain09 <tanish.jain.0905@gmail.com>
2025-09-01 07:35:42 +0530

    tree question

---
+++ src/dataStructures/Tree/Questions/ZigzagLevelOrder.java
@@ -0,0 +1,68 @@
+package dataStructures.Tree.Questions;
+
+import java.util.*;
+
+public class ZigzagLevelOrder {
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+
+            TreeNode() {
+            }
+
+            TreeNode(int val) {
+                this.val = val;
+            }
+
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+
+        }
+    public static void main(String[] args) {
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()){
+            int levelSize =  queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for(int i=0; i < levelSize; i++){
+                if(!reverse){
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if(currentNode.left != null){
+                        queue.addLast(currentNode.left);
+                    }
+                    if(currentNode.right != null){
+                        queue.addLast(currentNode.right);
+                    }
+                }else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
